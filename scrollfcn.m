32cef2dc38f122f459f2c53e871bbaff13133890
drewstev <astevens@usgs.gov>
2015-11-06 09:26:59 -0800

    fixed scrollfcn

--- scrollfcn.m
+++ scrollfcn.m
@@ -8,8 +8,8 @@ yl=get(gd.ax1,'ylim');
 
 xrange=diff(xl);
 yrange=diff(yl);
-gd.xlims=[xy(1)-(xrange/2) xy(1)+(xrange/2)].*1000;
-gd.ylims=[xy(2)-(yrange/2) xy(2)+(yrange/2)].*1000;
+gd.xlims=[xy(1)-(xrange/2) xy(1)+(xrange/2)];
+gd.ylims=[xy(2)-(yrange/2) xy(2)+(yrange/2)];
 
 
 
@@ -17,11 +17,11 @@ guidata(hf,gd);
 optimize_view(hf)
 
 gd=guidata(hf);
-set(gd.ph,'xdata',gd.xs./1000,...
-    'ydata',gd.ys./1000,...
+set(gd.ph,'xdata',gd.xs,...
+    'ydata',gd.ys,...
     'zdata',zeros(size(gd.zs)),...
     'cdata',gd.hs);
-set(gd.ax1,'xlim',gd.xlims./1000,'ylim',gd.ylims./1000);
+set(gd.ax1,'xlim',gd.xlims,'ylim',gd.ylims);
 
 
 set(gd.str,'string',['Displaying ',gd.filename, '  Resolution = ',...
